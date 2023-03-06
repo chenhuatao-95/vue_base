1be69caaffca6d02e6d5ffdd11437360f786b6ec
chtandzyt <126761345+chtandzyt@users.noreply.github.com>
2023-03-06 16:56:22 +0800

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>{{ state.message }}</h1>
+    <button @click="changeMsg">点击</button>
+    <button @click="test">点击+10</button>
+  </div>
+</template>
+<script>
+import { reactive, ref } from "vue";
+export default {
+  name: "AboutView",
+  props: {
+    msg: {
+      type: Number,
+    },
+  },
+  setup(props, context) {
+    var state = reactive({
+      message: props.msg,
+      title: "about",
+    });
+    console.log(props, "父组件传来的值");
+    function changeMsg() {
+      context.emit("getMsg", state.title);
+    }
+    function test() {
+      state.message += 10;
+    }
+    return {
+      changeMsg,
+      test,
+      state,
+    };
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <AboutView :msg="state.msg" @getMsg="getMsg"></AboutView>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+import AboutView from "@/views/AboutView.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    AboutView,
+  },
+  setup() {
+    var state = reactive({
+      msg: 1,
+    });
+    function getMsg(val) {
+      console.log(val, '子组件传来的值')
+    }
+    return {
+      state,
+      getMsg
+    };
+  },
+};
+</script>
